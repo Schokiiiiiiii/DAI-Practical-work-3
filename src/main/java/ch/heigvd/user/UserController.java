3ddabc78f6cf70b9aa805ae61dfe2dc526941610
Schokiiiiiiii <fabien@leger-dev.swiss>
2026-01-07 15:43:10 +0100

    Added TODO for clarity

--- src/main/java/ch/heigvd/user/UserController.java
+++ src/main/java/ch/heigvd/user/UserController.java
@@ -6,26 +6,27 @@ import io.javalin.http.*;
 public class UserController {
 
     public void create(Context ctx) {
-
+        // TODO implement
 
     }
 
     public void getAll(Context ctx) {
-
+        // TODO implement
 
     }
 
     public void getOne(Context ctx) {
-
+        // TODO implement
 
     }
 
     public void update(Context ctx) {
-
+        // TODO implement
 
     }
 
     public void delete(Context ctx) {
+        // TODO implement
 
     }
 }
