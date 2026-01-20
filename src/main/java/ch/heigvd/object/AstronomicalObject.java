cb3f4b51a63c60bc6ebd8f3e8013d07c619f9c6f
Schokiiiiiiii <fabien@leger-dev.swiss>
2026-01-20 19:25:35 +0100

    Finished fixing bugs

--- src/main/java/ch/heigvd/object/AstronomicalObject.java
+++ src/main/java/ch/heigvd/object/AstronomicalObject.java
@@ -1,44 +1,21 @@
 package ch.heigvd.object;
 
-public class AstronomicalObject {
-
-    private final Integer id;
-    private final String name;
-    private final String type;
-    private final int diameter;
-    private final double mass;
-    private final double escape_velocity;
-    private final double surface_temperature;
-    private String created_by;
-
-    AstronomicalObject(Integer id,
-                       String name,
-                       String type,
-                       int diameter,
-                       double mass,
-                       double escape_velocity,
-                       double surface_temperature,
-                       String created_by) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.diameter = diameter;
-        this.mass = mass;
-        this.escape_velocity = escape_velocity;
-        this.surface_temperature = surface_temperature;
-        this.created_by = created_by;
-    }
-
-    public Integer id() { return id; }
-    public String name() { return name; }
-    public String type() { return type; }
-    public int diameter() { return diameter; }
-    public double mass() { return mass; }
-    public double escape_velocity() { return escape_velocity; }
-    public double surface_temperature() { return surface_temperature; }
-    public String created_by() { return created_by; }
-
-    public void setCreatedByToNull() {
-        created_by = null;
+public record AstronomicalObject(Integer id,
+                                 String name,
+                                 String type,
+                                 int diameter,
+                                 double mass,
+                                 double escape_velocity,
+                                 double surface_temperature,
+                                 String created_by) {
+    public AstronomicalObject copyWithNullCreator() {
+        return new AstronomicalObject(this.id(),
+                this.name(),
+                this.type(),
+                this.diameter(),
+                this.mass(),
+                this.escape_velocity(),
+                this.surface_temperature(),
+                null);
     }
 }
--- src/main/java/ch/heigvd/user/UserController.java
+++ src/main/java/ch/heigvd/user/UserController.java
@@ -1,9 +1,11 @@
 package ch.heigvd.user;
 
-// JAVALIN
 import ch.heigvd.object.AstronomicalObject;
 import io.javalin.http.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
 
 public class UserController {
@@ -23,7 +25,7 @@ public class UserController {
 
         // look for any other similar names
         for (User user : users.values())
-            if (newUser.username().equalsIgnoreCase(user.email()))
+            if (newUser.username().equalsIgnoreCase(user.username()))
                 throw new ConflictResponse();
 
         // create
@@ -35,47 +37,64 @@ public class UserController {
                 newUser.biography()
         );
 
+        // add user to database
         users.put(newUser.username(), newUser);
 
+        // send status and user back
         ctx.status(HttpStatus.CREATED);
         ctx.json(newUser);
     }
 
     public void getAll(Context ctx) {
-        ctx.json(users);
+
+        // put all users into a list
+        List<User> result = new ArrayList<>(users.values());
+
+        // return the list of users
+        ctx.json(result);
     }
 
     public void getOne(Context ctx) {
 
+        // get username from parameter
         String username = ctx.pathParamAsClass("username", String.class).get();
 
+        // retrieve user inside the database
         User user = users.get(username);
 
+        // check if not found
         if (user == null) {
             throw new NotFoundResponse();
         }
 
+        // send user back
         ctx.json(user);
     }
 
     public void update(Context ctx) {
 
+        // get username from parameter
         String username = ctx.pathParamAsClass("username", String.class).get();
 
+        // check the user exists in the database
         if (!users.containsKey(username)) {
             throw new NotFoundResponse();
         }
 
+        // get updated user
         User updateUser = validateBody(ctx);
 
+        // check the username is unique
         for (User user : users.values()) {
             if (updateUser.username().equalsIgnoreCase(user.username())) {
                 throw new ConflictResponse();
             }
         }
 
+        // put user inside the database
         users.put(username, updateUser);
 
+        // return updated user
         ctx.json(updateUser);
     }
 
@@ -90,11 +109,17 @@ public class UserController {
         }
 
         // remove user on astronomical objects
-        for (AstronomicalObject object : objects.values()) {
-            if (object.created_by().equals(object.name())) {
-                object.setCreatedByToNull();
+        for (Map.Entry<Integer, AstronomicalObject> entry : objects.entrySet()) {
+            AstronomicalObject obj = entry.getValue();
+
+            if (username.equals(obj.created_by())) {
+                AstronomicalObject updated = obj.copyWithNullCreator();
+
+                // replace the object
+                objects.replace(entry.getKey(), obj, updated);
             }
         }
+
 
         // remove user from database
         users.remove(username);
