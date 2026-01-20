e6f95f6f1d78399f008404fff363c743a5b00482
Schokiiiiiiii <fabien@leger-dev.swiss>
2026-01-20 20:31:16 +0100

    Fixed formatting problems

--- src/main/java/ch/heigvd/object/AstronomicalObject.java
+++ src/main/java/ch/heigvd/object/AstronomicalObject.java
@@ -1,21 +1,23 @@
 package ch.heigvd.object;
 
-public record AstronomicalObject(Integer id,
-                                 String name,
-                                 String type,
-                                 int diameter,
-                                 double mass,
-                                 double escape_velocity,
-                                 double surface_temperature,
-                                 String created_by) {
-    public AstronomicalObject copyWithNullCreator() {
-        return new AstronomicalObject(this.id(),
-                this.name(),
-                this.type(),
-                this.diameter(),
-                this.mass(),
-                this.escape_velocity(),
-                this.surface_temperature(),
-                null);
-    }
+public record AstronomicalObject(
+    Integer id,
+    String name,
+    String type,
+    int diameter,
+    double mass,
+    double escape_velocity,
+    double surface_temperature,
+    String created_by) {
+  public AstronomicalObject copyWithNullCreator() {
+    return new AstronomicalObject(
+        this.id(),
+        this.name(),
+        this.type(),
+        this.diameter(),
+        this.mass(),
+        this.escape_velocity(),
+        this.surface_temperature(),
+        null);
+  }
 }
